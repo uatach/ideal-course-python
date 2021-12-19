d855c5c20128893796abbbe5bcce9c9d4dcf8012
edson duarte <edsonduarte1990@gmail.com>
2021-12-19 10:20:00 -0300

    refactor code for lesson 2

--- 020-main.py
+++ 020-main.py
@@ -13,92 +13,89 @@ class Result:
 
 @attr.s
 class Interaction:
-    label = attr.ib()
-    experience = attr.ib(None)
-    result = attr.ib(None)
-    valence = attr.ib(None)
+    experiment = attr.ib()
+    result = attr.ib()
+    valence = attr.ib()
+
+    @property
+    def label(self):
+        return self.experiment.label + self.result.label
+
+
+@attr.s
+class Environment:
+    results = dict()
+
+    def get_result(self, label: str) -> Result:
+        return self.results.setdefault(label, Result(label))
+
+    def perform(self, experiment: Experiment) -> Result:
+        if experiment == Experiment("e1"):
+            return self.get_result("r1")
+        return self.get_result("r2")
 
 
 @attr.s
 class Existence:
-    mood = attr.ib(None)
-    experiences = attr.ib(factory=dict)
-    interactions = attr.ib(factory=dict)
-    results = attr.ib(factory=dict)
+    env = attr.ib()
+
+    mood = None
+    experience = None
+    experiments = dict()
+    interactions = dict()
 
     def __attrs_post_init__(self):
-        e1 = self.get_experience("e1")
-        e2 = self.get_experience("e2")
-        r1 = self.get_result("r1")
-        r2 = self.get_result("r2")
-        self.setup_interaction(e1, r1, -1)
-        self.setup_interaction(e1, r2, 1)
-        self.setup_interaction(e2, r1, -1)
-        self.setup_interaction(e2, r2, 1)
-        self.prev_experience = e1
+        e1 = self.get_experiment("e1")
+        e2 = self.get_experiment("e2")
+        r1 = self.env.get_result("r1")
+        r2 = self.env.get_result("r2")
+        self.get_interaction(e1, r1, -1)
+        self.get_interaction(e1, r2, 1)
+        self.get_interaction(e2, r1, -1)
+        self.get_interaction(e2, r2, 1)
+        self.experience = e1
 
-    def get_experience(self, label: str) -> Experiment:
-        if label not in self.experiences:
-            self.experiences[label] = Experiment(label)
-        return self.experiences[label]
+    def get_experiment(self, label: str) -> Experiment:
+        return self.experiments.setdefault(label, Experiment(label))
 
-    def get_result(self, label: str) -> Result:
-        if label not in self.results:
-            self.results[label] = Result(label)
-        return self.results[label]
+    def get_interaction(
+        self, experiment: Experiment, result: Result, valence: int
+    ) -> Interaction:
+        interaction = Interaction(experiment, result, valence)
+        return self.interactions.setdefault(interaction.label, interaction)
 
-    def get_interaction(self, label: str) -> Interaction:
-        if label not in self.interactions:
-            self.interactions[label] = Interaction(label)
-        return self.interactions[label]
-
-    def swap_experience(self, experience: Experiment) -> Experiment:
-        for e in self.experiences.values():
-            if e != experience:
+    def swap(self, experiment: Experiment) -> Experiment:
+        for e in self.experiments.values():
+            if e != experiment:
                 return e
 
-    def create_result(self, experience: Experiment) -> Result:
-        if experience == self.get_experience("e1"):
-            return self.get_result("r1")
-        return self.get_result("r2")
-
-    def setup_interaction(
-        self, experience: Experiment, result: Result, valence: int = None
-    ) -> Interaction:
-        interaction = self.get_interaction(experience.label + result.label)
-        interaction.experience = experience
-        interaction.result = result
-        if valence:
-            interaction.valence = valence
-        return interaction
-
-    def predict_result(self, experience: Experiment) -> Result:
+    def find_interaction(self, experiment: Experiment, result: Result) -> Interaction:
         for i in self.interactions.values():
-            if i.experience == experience:
-                return i.result
-        return None
+            if i.experiment == experiment and i.result == result:
+                return i
 
     def step(self) -> str:
-        experience = self.prev_experience
+        experiment = self.experience
 
         if self.mood == "pained":
-            experience = self.swap_experience(experience)
+            experiment = self.swap(experiment)
 
-        result = self.create_result(experience)
-        interaction = self.setup_interaction(experience, result)
+        result = self.env.perform(experiment)
+        interaction = self.find_interaction(experiment, result)
 
         if interaction.valence > 0:
             self.mood = "pleased"
         else:
             self.mood = "pained"
 
-        self.prev_experience = experience
+        self.experience = experiment
 
-        return f"{experience.label}{result.label} {self.mood}"
+        return f"{interaction.label} {self.mood}"
 
 
 if __name__ == "__main__":
-    existence = Existence()
+    env = Environment()
+    existence = Existence(env)
 
     for i in range(20):
         trace = existence.step()
